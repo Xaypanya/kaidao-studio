e8223ccf07df648654fbee3ea7843195ea800377
Xaypanya Phongsa <dackogan@gmail.com>
2023-03-17 02:55:41 +0700

    add warning sign like facebook in console

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+console.log('%cຢ່າໆໆ!', 'color: red; font-size: 30px; font-weight: bold;');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
