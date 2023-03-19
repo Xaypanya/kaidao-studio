a1f97ea0c183d0efd738a3a4cfe684503f61af14
Xaypanya Phongsa <dackogan@gmail.com>
2023-03-20 01:54:08 +0700

    hide base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kaidao-studio/',
+  // base: '/kaidao-studio/',
   publicDir: './src/public',
   // root: 'src',
   build: {
